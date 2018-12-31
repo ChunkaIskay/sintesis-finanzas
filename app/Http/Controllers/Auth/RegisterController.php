8c1477e7c3dbf489963432e9796aa4b303254d89
Virus <fenixtres@hotmail.com>
2018-12-31 19:56:45 -0400

    Advances Register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/list-contract';
 
     /**
      * Create a new controller instance.
