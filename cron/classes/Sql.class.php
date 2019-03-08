06b49bd8c04d721404f79e5fea371f08690f41f4
Virus <fenixtres@hotmail.com>
2019-03-08 18:21:09 -0400

    test4

--- cron/classes/Sql.class.php
+++ cron/classes/Sql.class.php
@@ -33,8 +33,8 @@ class Sql extends Setup
             $obj = new Setup();
             $conectDB = $obj->conectDataB(1);
   
-            $fecha = "2018-12-16";
-            $fecha1 = "2018-12-23";
+            $fecha = "2018-12-24";
+            $fecha1 = "2018-12-31";
             $rs1 = array();
 
           for ($opcion=3;$opcion<=125; $opcion++)
