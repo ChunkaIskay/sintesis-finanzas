43e5e603ea3d1816b9b3e62e16fb96ce1e17bac2
Virus <fenixtres@hotmail.com>
2019-03-11 18:51:38 -0400

    test5

--- app/Http/Controllers/TransactionImportController.php
+++ app/Http/Controllers/TransactionImportController.php
@@ -12,11 +12,330 @@ use App\TransactionImport;
 class TransactionImportController extends Controller
 {
 	public function index(){
-	 	
-
-	 	return view('comission.index',  array('contacts' => 'hola'));
-	
-
+		echo "<br> JHALEA:". $this->jhalea();
+		echo "<br> MI RACHO:". $this->mi_rancho();
+		echo "<br> TIERRA:". $this->tierra();
+		echo "<br> CREDICASAS:". $this->credicasas();
+		echo "<br> cmp:". $this->cmp();
+		echo "<br> AXS:". $this->axs();
+		
+exit;
+	 	//return view('comission.index',  array('contacts' => 'hola'));
 	}
 	
+	/**
+	** Clàusula octava: (precio y forma de pago)
+	** Precio
+	** El Cliente se obliga a pagar al Proveedor por la prestaciòn del servicio, lo siguiente.
+	**    transacciones Mensuales    Cargo Fijo Mensual    Costo Unitario
+	**    Menores a 1,750                 bs. 5,250
+	**    de 1,751 a 5,000                                    bs. 2.80
+	**    de 5,001 a 15,000                                   bs. 2.50 
+	**    Mayor a 15,000									  bs. 2.20
+	**
+	**/
+
+	public function jhalea(){
+
+		$arrayPrices = array(
+						0 => array('from' => 0 ,'until'=>1750, 'monthlyFixed' => 5250, 'unitCost'=>0 ) ,
+						1 => array('from' => 1751 ,'until'=>5000, 'monthlyFixed' => 0, 'unitCost'=>2.80 ),
+						2 => array('from' => 5001 ,'until'=>15000, 'monthlyFixed' => 0, 'unitCost'=>2.50 ),
+						3 => array('from' => 15000 ,'until'=>0, 'monthlyFixed' => 0, 'unitCost'=>2.20 )
+						);
+
+		$totalTransaction = DB::table('transaction_import')
+	    ->where('servicio', 'like', "%TUPPERWARE-TUPPERWARE%")
+	    ->where('cli','=',76)
+	    ->sum('tot');
+		
+	    if ($totalTransaction > $arrayPrices[0]['from'] && $totalTransaction <= $arrayPrices[0]['until']){
+
+	    	$totalBilling = $arrayPrices[0]['monthlyFixed'];
+	    }
+	    if ($totalTransaction > $arrayPrices[1]['from'] && $totalTransaction <= $arrayPrices[1]['until']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[1]['unitCost'];
+	    }
+	    if ($totalTransaction > $arrayPrices[2]['from'] && $totalTransaction <= $arrayPrices[2]['until']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[2]['unitCost'];
+	    }
+	    if ($totalTransaction > $arrayPrices[3]['from']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[3]['unitCost'];
+	    }
+
+	    return "Transacciones total: ".$totalTransaction. " total a cobrar: ". $totalBilling;
+//SELECT SUM(tot) FROM `transaction_import` WHERE servicio like ('%TUPPERWARE-TUPPERWARE%') ORDER BY enti ASC
+
+	} 
+
+/**
+	** 
+	** Precio
+	** El Cliente se obliga a pagar al Proveedor por la prestaciòn del servicio, lo siguiente.
+	**    transacciones Mensuales    Cargo Fijo Mensual    Costo Unitario
+	**    1-1200                 bs. 3480
+	**    de 1201 a 5,000                                    bs. 2.80
+	**    de 5,001 en adelante                               bs. 2.70 
+	**
+	**/
+
+	public function mi_rancho(){
+
+		$arrayPrices = array(
+						0 => array('from' => 1 ,'until'=>1200, 'monthlyFixed' => 3480, 'unitCost'=>0 ) ,
+						1 => array('from' => 1201 ,'until'=>5000, 'monthlyFixed' => 0, 'unitCost'=>2.80 ),
+						2 => array('from' => 5001 ,'until'=>0, 'monthlyFixed' => 0, 'unitCost'=>2.70 )
+						);
+
+		$totalTransaction = DB::table('transaction_import')
+	    ->where('servicio', 'like', "%NOVILLO-MI RANCHO%")
+	    ->where('cli','=',102)
+	    ->sum('tot');
+		
+	    if ($totalTransaction > $arrayPrices[0]['from'] && $totalTransaction <= $arrayPrices[0]['until']){
+
+	    	$totalBilling =  $arrayPrices[0]['monthlyFixed'];
+	    }
+	    if ($totalTransaction > $arrayPrices[1]['from'] && $totalTransaction <= $arrayPrices[1]['until']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[1]['unitCost'];
+	    }
+	   
+	    if ($totalTransaction >= $arrayPrices[2]['from']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[2]['unitCost'];
+	    }
+		
+		return "Transacciones total: ".$totalTransaction. " total a cobrar: ". $totalBilling;
+//SELECT SUM(tot) FROM `transaction_import` WHERE servicio like ('%TUPPERWARE-TUPPERWARE%') ORDER BY enti ASC
+	} 
+
+
+   /**
+	** Comisiòn por recaudaciòn
+	** Precio
+	** El Cliente se obliga a pagar al Proveedor por la prestaciòn del servicio, lo siguiente.
+	**    transacciones Mensuales    Cargo Fijo Mensual    Costo Unitario
+	**    De 1 a 1,200                 bs. 3480
+	**    de 1,201 a 5,000                                    bs. 2.80
+	**    Mayores a 5,001                                     bs. 2.70 
+	**
+	**/
+
+	public function tierra(){
+
+		$arrayPrices = array(
+						0 => array('from' => 1 ,'until'=>1200, 'monthlyFixed' => 3480, 'unitCost'=>0 ) ,
+						1 => array('from' => 1201 ,'until'=>5000, 'monthlyFixed' => 0, 'unitCost'=>2.80 ),
+						2 => array('from' => 5001 ,'until'=>0, 'monthlyFixed' => 0, 'unitCost'=>2.70 )
+						);
+
+		$totalTransaction = DB::table('transaction_import')
+	    ->where('servicio', 'like', "%NOVILLO-TIERRA QUINTA%")
+	    ->where('cli','=',102)
+	    ->sum('tot');
+		
+	    if ($totalTransaction > $arrayPrices[0]['from'] && $totalTransaction <= $arrayPrices[0]['until']){
+
+	    	$totalBilling = $arrayPrices[0]['monthlyFixed'];
+	    }
+	    if ($totalTransaction > $arrayPrices[1]['from'] && $totalTransaction <= $arrayPrices[1]['until']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[1]['unitCost'];
+	    }
+	    if ($totalTransaction >= $arrayPrices[2]['from']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[2]['unitCost'];
+	    }
+
+	    return "Transacciones total: ".$totalTransaction. " total a cobrar: ". $totalBilling;
+//SELECT SUM(tot) FROM `transaction_import` WHERE servicio like ('%TUPPERWARE-TUPPERWARE%') ORDER BY enti ASC
+	} 
+
+ /**
+	** Comisiòn por recaudaciòn
+	** a.- Comisiòn fija mensual bs 3480 bolivianos. Que cubre hasta 1200 transacciones, una vez sobrepasadas las 1200 transacciones se cobrarà ùnicamente por transacciòn realizada segùn la siguiente tabla.
+	** 
+	**    transacciones Mensuales    Cargo Fijo Mensual    Costo Unitario
+	**    De 1 a 1,200                 bs. 3480
+	**    de 1,201 a 5,000                                    bs. 2.80
+	**    Mayores a 5,001                                     bs. 2.70 
+	**
+	**/
+
+	public function credicasas(){
+
+		$arrayPrices = array(
+						0 => array('from' => 1 ,'until'=>1200, 'monthlyFixed' => 3480, 'unitCost'=>0 ) ,
+						1 => array('from' => 1201 ,'until'=>5000, 'monthlyFixed' => 0, 'unitCost'=>2.80 ),
+						2 => array('from' => 5001 ,'until'=>0, 'monthlyFixed' => 0, 'unitCost'=>2.70 )
+						);
+
+		$totalTransaction = DB::table('transaction_import')
+	    ->where('servicio', 'like', "%NOVILLO-CREDICASAS%")
+	    ->where('cli','=',102)
+	    ->sum('tot');
+		
+	    if ($totalTransaction > $arrayPrices[0]['from'] && $totalTransaction <= $arrayPrices[0]['until']){
+
+	    	$totalBilling = $arrayPrices[0]['monthlyFixed'];
+	    }
+	    if ($totalTransaction > $arrayPrices[1]['from'] && $totalTransaction <= $arrayPrices[1]['until']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[1]['unitCost'];
+	    }
+	    if ($totalTransaction >= $arrayPrices[2]['from']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[2]['unitCost'];
+	    }
+
+	    return "Transacciones total: ".$totalTransaction. " total a cobrar: ". $totalBilling;
+//SELECT SUM(tot) FROM `transaction_import` WHERE servicio like ('%TUPPERWARE-TUPPERWARE%') ORDER BY enti ASC
+	} 
+
+	/**
+	** 
+	** Precio
+	**    transacciones Mensuales    Cargo Fijo Mensual    Costo Unitario
+	**    1-1,000                                              bs. 4,50
+	**    de 1,001 a 5,000                                     bs. 3,90
+	**    de 5,001 a 15,000                                    bs. 3.70 
+	**    15001- Adelante									   bs. 3.50
+	**    CONSTO ADICIONAL  IMPRESIÒN DE FACTURA               bs. 0.70
+	**/
+
+	public function cmp(){
+
+		$arrayPrices = array(
+						0 => array('from' => 1 ,'until'=>1000, 'monthlyFixed' => 0, 'unitCost'=>4.50, 'additionalCostBilling' =>0.70 ) ,
+						1 => array('from' => 1001 ,'until'=>5000, 'monthlyFixed' => 0, 'unitCost'=>3.90 ),
+						2 => array('from' => 5001 ,'until'=>15000, 'monthlyFixed' => 0, 'unitCost'=>3.70 ),
+						3 => array('from' => 15001 ,'until'=>0, 'monthlyFixed' => 0, 'unitCost'=>3.50 )
+						);
+
+		$totalTransaction = DB::table('transaction_import')
+	    ->where('servicio', 'like', "%MEMPARK-CMP CREDITO%")
+	    ->where('cli','=',83)
+	    ->sum('tot');
+		
+	    if ($totalTransaction > $arrayPrices[0]['from'] && $totalTransaction <= $arrayPrices[0]['until']){
+
+	    	$totalBilling1 =  $totalTransaction * $arrayPrices[0]['unitCost'];
+	    	$totalBilling2 =  $totalTransaction * $arrayPrices[0]['additionalCostBilling'] ;
+	    	$totalBilling = $totalBilling1 + $totalBilling1;
+		}
+	    if ($totalTransaction > $arrayPrices[1]['from'] && $totalTransaction <= $arrayPrices[1]['until']){
+
+	    	$totalBilling1 =  $totalTransaction * $arrayPrices[1]['unitCost'];
+	    	$totalBilling2 =  $totalTransaction * $arrayPrices[0]['additionalCostBilling'] ;
+	    	$totalBilling = $totalBilling1 + $totalBilling2;
+	    }
+	    if ($totalTransaction > $arrayPrices[2]['from'] && $totalTransaction <= $arrayPrices[2]['until']){
+
+	    	$totalBilling1 =  $totalTransaction * $arrayPrices[2]['unitCost'];
+	    	$totalBilling2 =  $totalTransaction * $arrayPrices[0]['additionalCostBilling'] ;
+	    	$totalBilling = $totalBilling1 + $totalBilling2;
+	    }
+	    if ($totalTransaction >= $arrayPrices[3]['from']){
+
+	    	$totalBilling1 =  $totalTransaction * $arrayPrices[3]['unitCost'];
+	    	$totalBilling2 =  $totalTransaction * $arrayPrices[0]['additionalCostBilling'] ;
+	    	$totalBilling = $totalBilling1 + $totalBilling2;
+	    }
+
+	    return "Transacciones total: ".$totalTransaction. " total a cobrar: ". $totalBilling;
+//SELECT SUM(tot) FROM `transaction_import` WHERE servicio like ('%TUPPERWARE-TUPPERWARE%') ORDER BY enti ASC
+
+	} 
+
+
+	 /**
+	** Comisiòn por recaudaciòn
+	** Precio
+	**    transacciones Mensuales    Cargo Fijo Mensual    Costo Unitario
+	**    De 1 a 5000                    bs. 1250
+	**    5001-10000                                          bs. 1.80
+	**    10001- ADELANTE                                     bs. 1.50 
+	**
+	**/
+
+	public function axs(){
+
+		$arrayPrices = array(
+						0 => array('from' => 1 ,'until'=>5000, 'monthlyFixed' => 1250, 'unitCost'=>0 ) ,
+						1 => array('from' => 5001 ,'until'=>10000, 'monthlyFixed' => 0, 'unitCost'=>1.80 ),
+						2 => array('from' => 10001 ,'until'=>0, 'monthlyFixed' => 0, 'unitCost'=>1.50 )
+						);
+
+		$totalTransaction = DB::table('transaction_import')
+	    ->where('servicio', 'like', "%AXS-AXS%")
+	    ->where('cli','=',34)
+	    ->sum('tot');
+		
+	    if ($totalTransaction > $arrayPrices[0]['from'] && $totalTransaction <= $arrayPrices[0]['until']){
+
+	    	$totalBilling = $arrayPrices[0]['monthlyFixed'];
+	    }
+	    if ($totalTransaction > $arrayPrices[1]['from'] && $totalTransaction <= $arrayPrices[1]['until']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[1]['unitCost'];
+	    }
+	    if ($totalTransaction >= $arrayPrices[2]['from']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[2]['unitCost'];
+	    }
+
+	    return "Transacciones total: ".$totalTransaction. " total a cobrar: ". $totalBilling;
+//SELECT SUM(tot) FROM `transaction_import` WHERE servicio like ('%TUPPERWARE-TUPPERWARE%') ORDER BY enti ASC
+	}
+
+	 /**
+	** La retribución y forma de pago
+	** l. La retribuciòn de los servicios, considerarà los siguiente:
+		Monto minimo
+	**    transacciones Mensuales    Cargo Fijo Mensual    Costo Unitario
+	**    De 1 a 5000                    bs. 10,000
+	**    5001 en adelante                                    bs. 1.59
+	**    
+	**    Transaciones minimas    de  1667                    bs. 2.00
+	**/
+
+	public function misiones(){
+
+		$arrayPrices = array(
+						0 => array('from' => 1 ,'until'=>5000, 'monthlyFixed' => 10000, 'unitCost'=>0, 'minimumTransactions' => 1667, 'unitCostMin'=>2.00 ),
+						1 => array('from' => 5001 ,'until'=>0, 'monthlyFixed' => 0, 'unitCost'=>1.59 )
+						);
+
+		$totalTransaction = DB::table('transaction_import')
+	    ->where('servicio', 'like', "%KANTUTANI-LAS MISIONES%")
+	    ->where('cli','=',40)
+	    ->sum('tot');
+
+	    // T. minimas sea menor a T. total
+
+	    $minimumTransactions = $arrayPrices[0]['minimumTransactions'] * $arrayPrices[0]['unitCostMin'];
+	    $additonialTransactions1 = $totalTransaction - $arrayPrices[0]['minimumTransactions'];
+	    $additonialTransactions = $additonialTransactions1 * $arrayPrices[1]['unitCost'];
+
+	    if ($totalTransaction > $arrayPrices[0]['from'] && $totalTransaction <= $arrayPrices[0]['until']){
+
+	    	$totalBilling = $arrayPrices[0]['monthlyFixed'];
+	    }
+	    if ($totalTransaction > $arrayPrices[1]['from'] && $totalTransaction <= $arrayPrices[1]['until']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[1]['unitCost'];
+	    }
+	    if ($totalTransaction >= $arrayPrices[2]['from']){
+
+	    	$totalBilling = $totalTransaction * $arrayPrices[2]['unitCost'];
+	    }
+
+	    return "Transacciones total: ".$totalTransaction. " total a cobrar: ". $totalBilling;
+//SELECT SUM(tot) FROM `transaction_import` WHERE servicio like ('%TUPPERWARE-TUPPERWARE%') ORDER BY enti ASC
+	}
+
 }
