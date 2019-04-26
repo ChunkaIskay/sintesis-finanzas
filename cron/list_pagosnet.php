998a57d2c3551a7cecf275cb7eb8801cb5669868
Virus <fenixtres@hotmail.com>
2019-04-26 14:27:14 -0400

    test145

--- cron/list_pagosnet.php
+++ cron/list_pagosnet.php
@@ -21,6 +21,6 @@
 	$fechaDesde = date('Y-m-d',$calcularFecha);
 	
 	$fechaHasta = date('Y-m-d',strtotime($fecha));
-	$obj->loadDataReport('2019-01-01','2019-01-31');
+	$obj->loadDataReport('2019-02-01','2019-02-28');
 	//resto 1 mes
 	//date("m",strtotime($fecha."- 1 month"));
