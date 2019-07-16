774d9cb839466d112253dfaed518101c52f1e689
Virus <fenixtres@hotmail.com>
2019-07-16 10:19:52 -0400

    test149

--- cron/cron_reports.php
+++ cron/cron_reports.php
@@ -24,8 +24,8 @@
         $fechaDesde = $fechaAntes5;
       
     }
-/*$fechaDesde = '2019-04-01';
-$fechaHasta = '2019-04-30';*/
+$fechaDesde = '2019-06-01';
+$fechaHasta = '2019-06-30';
 
     $obj->importData($fechaDesde, $fechaHasta); 
     //resto 1 mes
